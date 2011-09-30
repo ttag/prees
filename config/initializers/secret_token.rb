4675662280b03d508e5d060cc5ffc32a8316310e
Your Name <x.eshan@hotmail.com>
2011-09-30 06:35:55 -0400

    spree + wishlist

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeWithGem::Application.config.secret_token = '5545f28e8c4b4cf0fc839782604ba49b05b4fdfd7744155dc0d5a2ab5fc50a51f0bbd92e9872374bff49b35faf7b7d8beb052fc9b1601e4e6d43639d523db0ee'
